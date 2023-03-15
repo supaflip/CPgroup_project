547a3f5def3c0c03ef13bd543d1f4bd1d55bfb4e
earno12 <earnold2010@gmail.com>
2023-03-14 21:11:41 -0500

    Updated nested accordion to have 10 weeks and 5 days

--- frontend/src/components/NestedAccordion.jsx
+++ frontend/src/components/NestedAccordion.jsx
@@ -3,7 +3,7 @@ import Accordion from 'react-bootstrap/Accordion';
 function NestedAccordion() {
   return (
     <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
+      <Accordion.Item eventKey="-1">
         <Accordion.Header>Week 1</Accordion.Header>
         <Accordion.Body>
           <Accordion defaultActiveKey="0">
@@ -19,19 +19,358 @@ function NestedAccordion() {
                 Day 2 Workout details...
               </Accordion.Body>
             </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Week 2</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Week 3</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
           </Accordion>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="3">
-        <Accordion.Header>Week 2</Accordion.Header>
+        <Accordion.Header>Week 4</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="4">
+        <Accordion.Header>Week 5</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="5">
+        <Accordion.Header>Week 6</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="6">
+        <Accordion.Header>Week 7</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="7">
+        <Accordion.Header>Week 8</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="8">
+        <Accordion.Header>Week 9</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="9">
+        <Accordion.Header>Week 10</Accordion.Header>
+        <Accordion.Body>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Day 1</Accordion.Header>
+              <Accordion.Body>
+                Day 1 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Day 2</Accordion.Header>
+              <Accordion.Body>
+                Day 2 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>Day 3</Accordion.Header>
+              <Accordion.Body>
+                Day 3 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>Day 4</Accordion.Header>
+              <Accordion.Body>
+                Day 4 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Day 5</Accordion.Header>
+              <Accordion.Body>
+                Day 5 Workout details...
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
