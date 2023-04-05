795c97778601be4b8928c9e0f3e08d6434736647
Chris Rances <chris.rances@outlook.com>
2023-04-04 22:34:52 -0400

    Delete Days working

--- frontend/src/components/DeleteModal.jsx
+++ frontend/src/components/DeleteModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import axios from "axios";
 import DeleteWeekForm from "./DeleteWeekForm";
+import DeleteDayForm from "./DeleteDayForm";
 import "../index.css";
 
 const DeleteModal = ({ data, onClose }) => {
@@ -12,23 +13,39 @@ const DeleteModal = ({ data, onClose }) => {
     week_number: item.week_number,
   }));
 
+  // Extract days information from data
+  const days = data.flatMap((item) =>
+    item.days.map((day) => ({
+      week_number: item.week_number,
+      day_number: day.day_number,
+    }))
+  );
+
+  // Separate states for each form, so each form can be submitted independently based on whether there is input data or not
   const [weekFormData, setWeekFormData] = useState({
     week_number: "",
   });
+  const [dayFormData, setDayFormData] = useState({ week: "", day_number: "" });
 
+  // Separate handleChanges functions for each form
   const handleWeekChange = (e) => {
     setWeekFormData({ ...weekFormData, [e.target.name]: e.target.value });
   };
+  const handleDayChange = (e) => {
+    setDayFormData({ ...dayFormData, [e.target.name]: e.target.value });
+    console.log("Selected day_number:", e.target.value);
+  };
 
+  // If statements in the handleSubmit function so each form can be submitted independently with just one Delete button
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("handleSubmit called") // delete when done testing
+    console.log("handleSubmit called"); // delete when done testing
     const token = localStorage.getItem("token");
 
     if (weekFormData.week_number) {
       console.log("Week number to delete:", weekFormData.week_number); // delete when done testing
       const userConfirmation = window.confirm(
-        "Do you really want to delete the entire Week?"
+        `Do you really want to delete the entire Week ${weekFormData.week_number}?`
       );
 
       if (userConfirmation) {
@@ -41,13 +58,36 @@ const DeleteModal = ({ data, onClose }) => {
               },
             }
           );
-          window.location.reload();
         } catch (err) {
           console.log(err);
         }
       }
     }
+
+    if (dayFormData.day_number) {
+      console.log("Day number to delete:", dayFormData.day_number); // delete when done testing
+      const userConfirmation = window.confirm(
+        `Do you really want to delete Day ${dayFormData.day_number.slice(2)} of Week ${dayFormData.week}?`
+      );
+
+      if (userConfirmation) {
+        try {
+          const response = await axios.delete(
+            `http://127.0.0.1:8000/workouts/day/${dayFormData.day_number}`,
+            {
+              headers: {
+                Authorization: "Token " + token,
+              },
+            }
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+
     onClose();
+    window.location.reload();
   };
 
   return (
@@ -70,12 +110,26 @@ const DeleteModal = ({ data, onClose }) => {
           weekFormData={weekFormData}
           handleChange={handleWeekChange}
         />
+        <hr />
+        <DeleteDayForm
+          weeks={weeks}
+          days={days}
+          dayFormData={dayFormData}
+          handleChange={handleDayChange}
+        />
+        <hr />
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onClose}>
           Cancel
         </Button>
-        <Button variant="primary" onClick={(e) => { console.log('Delete button clicked'); handleSubmit(e); }}>
+        <Button
+          variant="primary"
+          onClick={(e) => {
+            console.log("Delete button clicked");
+            handleSubmit(e);
+          }}
+        >
           Delete
         </Button>
       </Modal.Footer>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -33,8 +33,6 @@ function HomePage({ USER_AUTH }) {
     setShowCreateModal(false);
   };
 
-
-
   const handleOpenDeleteModal = () => {
     setShowDeleteModal(true);
   };
@@ -47,10 +45,22 @@ function HomePage({ USER_AUTH }) {
     <div>
       {!USER_AUTH["TOKEN"] && <Navigate to="/app" />}
       <h5>Dashboard</h5>
-      <Button className="mt-3" onClick={handleOpenCreateModal}>Create</Button>
-      {showCreateModal && <CreateModal data={weeks} onClose={handleCloseCreateModal} />}
-      <Button className="mt-3" variant="secondary" onClick={handleOpenDeleteModal}>Delete</Button>
-      {showDeleteModal && <DeleteModal data={weeks} onClose={handleCloseDeleteModal} />}
+      <Button className="mt-3" onClick={handleOpenCreateModal}>
+        Create
+      </Button>
+      {showCreateModal && (
+        <CreateModal data={weeks} onClose={handleCloseCreateModal} />
+      )}
+      <Button
+        className="mt-3"
+        variant="secondary"
+        onClick={handleOpenDeleteModal}
+      >
+        Delete
+      </Button>
+      {showDeleteModal && (
+        <DeleteModal data={weeks} onClose={handleCloseDeleteModal} />
+      )}
       <Routes>
         <Route
           path="/"
@@ -74,72 +84,6 @@ function HomePage({ USER_AUTH }) {
 }
 
 export default HomePage;
-
-
-
-// import React, { useState, useEffect } from "react";
-// import API from "../api/API";
-// import WeekList from "../components/WeekList";
-// import { Routes, Route } from "react-router-dom";
-// import WeekPage from "./WeekPage.jsx";
-// import { Navigate } from "react-router-dom";
-// import CreateModal from "../components/CreateModal";
-// import Button from "react-bootstrap/Button";
-
-// function HomePage({ USER_AUTH }) {
-//   const [weeks, setWeeks] = useState([]); // holds all week, day, and workout data in 1 file "weeks"
-
-//   useEffect(() => {
-//     const getWeeks = async () => {
-//       const data = await API.fetchWeeks();
-//       if (data) {
-//         setWeeks(data.result);
-//       }
-//     };
-//     getWeeks();
-//   }, []);
-
-//   const [showCreateModal, setShowCreateModal] = useState(false);
-
-//   const handleOpenCreateModal = () => {
-//     setShowCreateModal(true);
-//   };
-
-//   const handleCloseCreateModal = () => {
-//     setShowCreateModal(false);
-//   };
-
-//   return (
-//     <div>
-//       {!USER_AUTH["TOKEN"] && <Navigate to="/app" />}
-//       <Button className="mt-3" onClick={handleOpenCreateModal}>Create</Button>
-//       {showCreateModal && <CreateModal data={weeks} onClose={handleCloseCreateModal} />}
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={
-//             <div>
-//               <br></br>
-//               <hr />
-//               <WeekList weeks={weeks} />
-//             </div>
-//           }
-//         />
-//         <Route
-//           path="/week/:week_number/*"
-//           element={<WeekPage weeks={weeks} />}
-//         />
-//       </Routes>
-//       <br></br>
-//       <br></br>
-//     </div>
-//   );
-// }
-
-// export default HomePage;
-
-
-
 
 // ORIGINAL CODE BEFORE ADDING CRUD FUNCTIONALITY
 // import { useState, useEffect } from "react";
